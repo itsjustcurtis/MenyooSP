9f7ccbc8db7b2fe82ddcabbbedd37e770d083ad5
ThatRealSpicyDragon <158034338+ThatRealSpicyDragon@users.noreply.github.com>
2025-04-01 03:08:14 +0100

    Update GTAblip.h

--- Solution/source/Scripting/GTAblip.h
+++ Solution/source/Scripting/GTAblip.h
@@ -895,6 +895,8 @@ public:
 
 	void SetScale(float value);
 
+//New functions
+
 	void ShowCone(bool toggle, int hudColorIndex);
 
 	void SetDisplay(int displayId);
@@ -906,6 +908,8 @@ public:
 	void AddBlipForArea(float x, float y, float z, float width, float height);
 
 	void SyncRotationWithEntity(int entityHandle);
+
+//New functions end here
 
 	void ShowRoute(bool value);
 
