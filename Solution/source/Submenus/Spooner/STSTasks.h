e90241d42cb37d14891441ee0202495bb5559037
ThatRealSpicyDragon <Kyle.Br.Richardson@outlook.com>
2026-03-01 16:00:00 +0000

    Dashed out broken blip functions in VS instead of Github.

--- Solution/source/Submenus/Spooner/STSTasks.h
+++ Solution/source/Submenus/Spooner/STSTasks.h
@@ -82,11 +82,11 @@ namespace sub::Spooner
     		bool showCone = false;            
     		int hudColorIndex;      
 			bool isSelectableOnMap = true; // true = 2, false = 8
-    		int priority = 2;               
-    		int displayId;   
-    		bool syncRotation;  
+    		//int priority = 2;               
+    		//int displayId;   
+    		//bool syncRotation;  
 			// New functions
-			int displayMode = 2;
+			//int displayMode = 2;
 			// New functions end here
 			AddBlip();
 			void Run(void* ve) override;
